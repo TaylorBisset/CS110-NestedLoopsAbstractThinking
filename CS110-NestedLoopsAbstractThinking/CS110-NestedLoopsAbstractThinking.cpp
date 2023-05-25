92d4e02ed9681f934c2a2316ccde98c4cfb28085
Taylor Bisset <taylorbisset@gmail.com>
2023-05-25 10:51:26 -0700

    Added horizontal line print logic.

--- CS110-NestedLoopsAbstractThinking/CS110-NestedLoopsAbstractThinking.cpp
+++ CS110-NestedLoopsAbstractThinking/CS110-NestedLoopsAbstractThinking.cpp
@@ -7,7 +7,11 @@ int main()
     char horLn = 196, verLn = 179;
     for (int row = 0; row < 10; row++)
     {
-        
+        for (int row = 0; row <= 10 * 4; row++)
+        {
+            cout << horLn;
+        }
+        cout << endl;
     }
     cout << "\n\n";
 }
