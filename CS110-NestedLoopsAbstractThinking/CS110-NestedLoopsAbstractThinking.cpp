74f22185104ffdc4e574896ec557a7aa6b948ed5
Taylor Bisset <taylorbisset@gmail.com>
2023-05-25 11:06:14 -0700

    Print " $ " in even rows.

--- CS110-NestedLoopsAbstractThinking/CS110-NestedLoopsAbstractThinking.cpp
+++ CS110-NestedLoopsAbstractThinking/CS110-NestedLoopsAbstractThinking.cpp
@@ -7,11 +7,29 @@ int main()
     char horLn = 196, verLn = 179;
     for (int row = 0; row < 10; row++)
     {
+        // Prints horizontal lines.
         for (int row = 0; row <= 10 * 4; row++)
         {
             cout << horLn;
         }
         cout << endl;
+
+        // Prints "|R:C|" at each row and column interval.
+        for (int column = 0; column < 10; column++) 
+        {
+            cout << verLn;
+
+            // Grid print logic here.
+            if (row % 2 == 0)
+            {
+                cout << " $ ";
+            }
+            else
+                cout << "   ";
+        }
+
+        cout << verLn << endl;
+
     }
     cout << "\n\n";
 }
